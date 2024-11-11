ead1b71d28819ffd28e9147ca2d2d2337d64b99d
Jardin93 <Haasichiller@live.de>
2024-11-11 17:08:32 +0100

    Registrieren & Login
    Getestet und stabil.
    Immernoch kein @PersistenceContext für den EntityManagerFactory möglich

--- src/main/java/ipwa02/Service.java
+++ src/main/java/ipwa02/Service.java
@@ -1,11 +1,14 @@
 package ipwa02;
 
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Named("Service")
 @SessionScoped
@@ -19,7 +22,10 @@ public class Service implements Serializable
         this.einAusgabeListe = einAusgabeListe;
     }
 
-    private Personen angemeldetePerson;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("IPWA02_DB");
+    private EntityManager em = emf.createEntityManager();
+
+    private Personen angemeldetePerson = null;
 
     public Personen getAngemeldetePerson()
     {
@@ -35,17 +41,55 @@ public class Service implements Serializable
     }
     public String anmelden()
     {
-        // anmeldeprozess
+        try
+        {
+            angemeldetePerson = em.createQuery("SELECT p FROM Personen p WHERE p.username = :username", Personen.class)
+                    .setParameter("username", einAusgabeListe.get(0))
+                    .getSingleResult();
+            if (!Objects.equals(angemeldetePerson.getPassword(), einAusgabeListe.get(1)))
+            {
+                angemeldetePerson = null;
+            }
+        }
+        catch (Exception e) {
+            return "index.xhtml?faces-redirect=true";
+        }
         return "index.xhtml?faces-redirect=true";
     }
     public String abmelden()
     {
-        setAngemeldetePerson(null);
-        return "login.xhtml?faces-redirect=true";
+        angemeldetePerson = null;
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "LoginRegister.xhtml?faces-redirect=true";
     }
     public String registrieren()
     {
-        // registrierungsprozess
+        if (Objects.equals(einAusgabeListe.get(1), einAusgabeListe.get(2)))  //Passwort übereinstimmung
+        {
+            try  //Suche ob Benutzername vergeben ist
+            {
+                angemeldetePerson = em.find(Personen.class, einAusgabeListe.get(0));
+            }
+            catch (Exception e) {}
+            if (angemeldetePerson == null) //Benutzer anlegen
+            {
+                angemeldetePerson = new Personen();
+                angemeldetePerson.setUsername(einAusgabeListe.get(0));
+                angemeldetePerson.setPassword(einAusgabeListe.get(1));
+                angemeldetePerson.setTeam(einAusgabeListe.get(3));
+                angemeldetePerson.setRole(einAusgabeListe.get(4));
+                try { //Benutzer speichern
+                    em.getTransaction().begin();
+                    em.persist(angemeldetePerson);
+                    em.getTransaction().commit();
+                } catch (Exception e) {
+                    if (em.getTransaction().isActive()) {
+                        em.getTransaction().rollback(); // Rollback bei Fehler
+                    }
+                    angemeldetePerson = null;
+                }
+            }
+        }
         return "index.xhtml?faces-redirect=true";
     }
     public String divAufgabenListe()
@@ -68,11 +112,14 @@ public class Service implements Serializable
         return angemeldetePerson.getRole();
     }
 
-
-
     //************************Simulierte Daten werden hier im Konstruktor erstellt*****************************
     public Service()
     {
-
+        einAusgabeListe = new ArrayList<>();
+        // Initialisiere die Liste mit der Anzahl der benötigten Elementen
+        for (int i = 0; i < 10; i++)
+        {
+            einAusgabeListe.add("");
+        }
     }
 }
